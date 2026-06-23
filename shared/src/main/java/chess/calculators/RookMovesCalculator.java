1b26d9b2812966671cbd35d501568a340581b917
efoulk4 <evanfoulk4@gmail.com>
2026-06-23 00:58:46 -0600

    Init move calculators for better delegation

---
+++ shared/src/main/java/chess/calculators/RookMovesCalculator.java
@@ -0,0 +1,15 @@
+package chess.calculators;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.List;
+
+public class RookMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> CalculateLegalMoves(ChessBoard board, ChessPosition myPosition) {
+        return List.of();
+    }
+}
