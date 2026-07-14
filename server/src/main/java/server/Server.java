d7b98708f97a9225212098616733225918e64108
efoulk4 <evanfoulk4@gmail.com>
2026-07-14 01:22:43 -0600

    fixed error handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,38 +5,53 @@ import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import io.javalin.*;
+import io.javalin.http.Context;
+import model.AuthData;
+import model.UserData;
 import service.*;
 
+import java.util.Map;
+
 public class Server {
+    private final DataAccess dataAccess;
+    private final ClearService clearService;
+    private final UserService userService;
+    private final GameService gameService;
+    private final AuthService authService;
+    private final Gson gson;
 
     private final Javalin javalin;
 
     public Server() {
-        DataAccess dataAccess = new MemoryDataAccess();
-        ClearService clearService = new ClearService(dataAccess);
-        UserService userService = new UserService(dataAccess);
-        GameService gameService = new GameService(dataAccess);
-        AuthService authService = new AuthService(dataAccess);
-        Gson gson = new Gson();
+        dataAccess = new MemoryDataAccess();
+        clearService = new ClearService(dataAccess);
+        userService = new UserService(dataAccess);
+        gameService = new GameService(dataAccess);
+        authService = new AuthService(dataAccess);
+        gson = new Gson();
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
-                .delete("/db", (ctx) -> {
-                    clearService.clear();
-                    ctx.result("{}");
-                        });
+                .delete("/db", this::clearData)
+                .post("/user", this::addUser)
 
 
-        javalin.exception(BadRequestException.class, (e, ctx)  ->
-                ctx.status(400).json(e));
-        javalin.exception(UnauthorizedException.class, (e, ctx)  ->
-                ctx.status(401).json(e));
-        javalin.exception(AlreadyTakenException.class, (e, ctx)  ->
-                ctx.status(403).json(e));
-        javalin.exception(DataAccessException.class, (e, ctx)  ->
-                ctx.status(500).json(e));
-
+                .exception(BadRequestException.class, (e, ctx)  ->
+                        ctx.status(400).result(gson.toJson(Map.of("message", e.getMessage()))))
+                .exception(UnauthorizedException.class, (e, ctx)  ->
+                        ctx.status(401).result(gson.toJson(Map.of("message", e.getMessage()))))
+                .exception(AlreadyTakenException.class, (e, ctx)  ->
+                        ctx.status(403).result(gson.toJson(Map.of("message", e.getMessage()))))
+                .exception(DataAccessException.class, (e, ctx)  ->
+                        ctx.status(500).result(gson.toJson(Map.of("message", e.getMessage()))));
     }
-
+    public void clearData (Context ctx){
+        clearService.clear();
+        ctx.result("{}");
+    }
+    public void addUser (Context ctx){
+        UserData req = gson.fromJson(ctx.body(), UserData.class);
+        ctx.result(gson.toJson(userService.registerUser(req)));
+    }
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
