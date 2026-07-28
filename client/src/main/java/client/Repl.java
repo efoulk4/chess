38b51ce75a99da7b67831f1df16791f8a5d808e4
efoulk4 <evanfoulk4@gmail.com>
2026-07-28 12:04:15 -0600

    STATE enum and repl constructor

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,5 +1,22 @@
 package client;
 
 public class Repl {
+    private final ServerFacade facade;
+    private State state;
+    private final Session session = new Session();
+    private final PreLoginClient preLogin;
+    private final PostLoginClient postLogin;
+    private final GameClient gameClient;
+
+    public Repl(String serverUrl) {
+        this.facade = new ServerFacade(serverUrl);
+        this.state = State.PRELOGIN;
+        // hand the SAME facade and SAME session to each client:
+        this.preLogin  = new PreLoginClient(facade, session);
+        this.postLogin = new PostLoginClient(facade, session);
+        this.gameClient = new GameClient(facade, session);
+    }
+
 
 }
+
