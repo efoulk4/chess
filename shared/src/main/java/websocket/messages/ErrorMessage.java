40e97b4c7a966402f61efaa01dd1a8aec7c565df
efoulk4 <evanfoulk4@gmail.com>
2026-08-08 18:39:16 -0600

    subclass extensions

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,13 @@
+package websocket.messages;
+
+public class ErrorMessage extends ServerMessage{
+    private String errorMessage;
+    public ErrorMessage(String message) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
