9e48da31922dc002bf6c5f1b549a2a9b5914a3c7
efoulk4 <evanfoulk4@gmail.com>
2026-08-10 12:37:45 -0600

    wsFacade constructor

--- client/src/main/java/client/WebsocketFacade.java
+++ client/src/main/java/client/WebsocketFacade.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import exceptions.ResponseException;
 import jakarta.websocket.*;
 import jakarta.websocket.Session;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
@@ -26,19 +28,18 @@ public class WebsocketFacade extends Endpoint {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             this.wsSession = container.connectToServer(this, socketURI);
 
-            //set message handler
             this.wsSession.addMessageHandler(new MessageHandler.Whole<String>() {
                 @Override
                 public void onMessage(String message) {
-                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
                     var type = serverMessage.getServerMessageType();
-                    switch (type){
-                        case NOTIFICATION -> {}
-                        case LOAD_GAME -> {}
-                        case ERROR -> {}
-                        case null, default -> {};
-                    }
-                    serverMessageHandler.notify(notification);
+
+                    ServerMessage parsed = switch (type){
+                        case NOTIFICATION -> gson.fromJson(message, NotificationMessage.class);
+                        case LOAD_GAME -> gson.fromJson(message, LoadGameMessage.class);
+                        case ERROR -> gson.fromJson(message, ErrorMessage.class);
+                    };
+                    serverMessageHandler.notify(parsed);
                 }
             });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
